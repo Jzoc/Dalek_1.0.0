c7864bd782673b3c06fbc42533c0ec7d4d9447b0
gusgra <58302241+gusgra@users.noreply.github.com>
2019-12-18 18:33:42 +0100

    Update Dalek_main.py

--- Dalek_main.py
+++ Dalek_main.py
@@ -1,113 +1,99 @@
 import pygame as pg
 import re
 import random as rnd
+
+"""FIX ERROR"""
+"""B DEL I FUNKTIONER"""
+
 # -------- Pygame initializer and constants
 pg.init()
-# graphic constants
+# -------- Graphic constants
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 ORANGE = (255, 140, 0)
 GREEN = (0, 128, 0)
-# Defining the fonts used for end screens
-font1 = pg.font.Font(None, 60)
-font2 = pg.font.Font(None, 30)
-# The clock will be used to control how fast the screen updates
-clock = pg.time.Clock()
-# -------- Object Classes -----------
-# class for doctor, the player controlled object
-class Player(object):
+# -------- Defining fonts for game overlay --------
+BigFont = pg.font.Font(None, 60)
+SmallFont = pg.font.Font(None, 30)
+
+
+# -------- Object Classes --------
+# Class for walls
+class Wall(object):
+    wall_list = []
+    wall_pos_list = []
+
     def __init__(self, pos):
+        self.wall_list.append(self)
         self.rect = pg.Rect(pos[0], pos[1], 20, 20)
+        self.wall_pos_list.append((self.rect.x, self.rect.y))
+
+
+# Class for floor (used for Sonic Screwdriver)
+class Floor(object):
+    floor_list = []
+    floor_pos_list = []
+
+    def __init__(self, pos):
+        self.floor_list.append(self)
+        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
+        self.floor_pos_list.append((self.rect.x, self.rect.y))
+
+
+# Class for Junk
+class Junk(object):
+    junk_list = []
+    junk_pos_list = []
+
+    def __init__(self, pos):
+        self.junk_list.append(self)
+        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
+        self.junk_pos_list.append((self.rect.x, self.rect.y))
+
+
+# Class for Dalek
+class Dalek(object):
+    dalek_list = []
+
+    def __init__(self, pos, d_id):
+        self.d_id = d_id
+        self.rect = pg.Rect((pos[0], pos[1], 20, 20))
+        self.x_change = 20
+        self.y_change = 20
+        self.dalek_list.append(self)
+
+    def move_single_axis(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+        for wall in Wall.wall_list:
+            if self.rect.colliderect(wall.rect):
+                if dx > 0:
+                    self.rect.right = wall.rect.left
+                if dx < 0:
+                    self.rect.left = wall.rect.right
+                if dy > 0:
+                    self.rect.bottom = wall.rect.top
+                if dy < 0:
+                    self.rect.top = wall.rect.bottom
+
     def move(self, dx, dy):
-        """move along each axis separately"""
+        """Move along the Dalek"""
         if dx != 0:
             self.move_single_axis(dx, 0)
         if dy != 0:
             self.move_single_axis(0, dy)
-        """defines how each dalek moves to chase the doctor"""
-        for i, dalek in enumerate(dalek_list):
-            d_x = abs(self.rect.x - dalek.rect.x)
-            d_y = abs(self.rect.y - dalek.rect.y)
-            if not d_y == 0:
-                R = (d_x/d_y)
-            if d_x == 0: # The doctor and dalek are on the same level
-                if dalek.rect.x < self.rect.x:
-                    dalek.move(20, 0)
-                if dalek.rect.x > self.rect.x:
-                    dalek.move(-20, 0)
-                if dalek.rect.y < self.rect.y:
-                    dalek.move(0, 20)
-                if dalek.rect.y > self.rect.y:
-                    dalek.move(0, -20)
-            elif d_y == 0: # The doctor and dalek are above and below each other
-                if dalek.rect.x < self.rect.x:
-                    dalek.move(20, 0)
-                if dalek.rect.x > self.rect.x:
-                    dalek.move(-20, 0)
-                if dalek.rect.y < self.rect.y:
-                    dalek.move(0, 20)
-                if dalek.rect.y > self.rect.y:
-                    dalek.move(0, -20)
-            elif d_x > d_y: # the dalek is further to the side than above or below
-                r_x = rnd.randrange(1, d_x+1)
-                r_y = rnd.randrange(1, d_y+1)
-                r = (r_x/r_y)
-                if r > R:
-                    if dalek.rect.x < self.rect.x:
-                        dalek.move(20, 0)
-                    if dalek.rect.x > self.rect.x:
-                        dalek.move(-20, 0)
-                    if dalek.rect.y < self.rect.y:
-                        dalek.move(0, 20)
-                    if dalek.rect.y > self.rect.y:
-                        dalek.move(0, -20)
-                if r < R:
-                    if dalek.rect.x < self.rect.x:
-                        dalek.move(20, 0)
-                    elif dalek.rect.x > self.rect.x:
-                        dalek.move(-20, 0)
-                    elif dalek.rect.y < self.rect.y:
-                        dalek.move(0, 20)
-                    elif dalek.rect.y > self.rect.y:
-                        dalek.move(0, -20)
-            elif d_x < d_y: # the dalek is further above or below, than to the side
-                r_x = rnd.randrange(1, d_x+1)
-                r_y = rnd.randrange(1, d_y+1)
-                r = (r_x / r_y)
-                if r > R:
-                    if dalek.rect.x < self.rect.x:
-                        dalek.move(20, 0)
-                    elif dalek.rect.x > self.rect.x:
-                        dalek.move(-20, 0)
-                    elif dalek.rect.y < self.rect.y:
-                        dalek.move(0, 20)
-                    elif dalek.rect.y > self.rect.y:
-                        dalek.move(0, -20)
-                if r < R:
-                    if dalek.rect.x < self.rect.x:
-                        dalek.move(20, 0)
-                    if dalek.rect.x > self.rect.x:
-                        dalek.move(-20, 0)
-                    if dalek.rect.y < self.rect.y:
-                        dalek.move(0, 20)
-                    if dalek.rect.y > self.rect.y:
-                        dalek.move(0, -20)
-            elif d_x == d_y: # The doctor and dalek are on a diagonal
-                if dalek.rect.x < self.rect.x:
-                    dalek.move(20, 0)
-                if dalek.rect.x > self.rect.x:
-                    dalek.move(-20, 0)
-                if dalek.rect.y < self.rect.y:
-                    dalek.move(0, 20)
-                if dalek.rect.y > self.rect.y:
-                    dalek.move(0, -20)
+
+
+# Class for Doctor
+class Doctor(object):
+    def __init__(self, pos):
+        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
+
     def move_single_axis(self, dx, dy):
-        """move along one axis separately"""
         self.rect.x += dx
         self.rect.y += dy
-        """Check for collisions with walls, if we hit a wall, move away from wall"""
-        for wall in wall_list:
+        for wall in Wall.wall_list:
             if self.rect.colliderect(wall.rect):
                 if dx > 0:
                     self.rect.right = wall.rect.left
@@ -118,7 +104,7 @@ class Player(object):
                 if dy < 0:
                     self.rect.top = wall.rect.bottom
         """Check for collisions with junk, if we hit junk, move away from junk"""
-        for junk in junk_list:
+        for junk in Junk.junk_list:
             if self.rect.colliderect(junk.rect):
                 if dx > 0:
                     self.rect.right = junk.rect.left
@@ -128,68 +114,42 @@ class Player(object):
                     self.rect.bottom = junk.rect.top
                 if dy < 0:
                     self.rect.top = junk.rect.bottom
-# class for the dalek
-class Dalek(object):
-    def __init__(self, pos, id):
-        self.id = id
-        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
-        self.x_change = 20
-        self.y_change = 20
-        dalek_pos_list.append((self.rect.x, self.rect.y))
-        dalek_list.append(self)
+
     def move(self, dx, dy):
-        """move along each axis separately"""
+        """Move along the Dalek"""
         if dx != 0:
             self.move_single_axis(dx, 0)
         if dy != 0:
             self.move_single_axis(0, dy)
-    def move_single_axis(self, dx, dy):
-        """move along one axis separately"""
-        self.rect.x += dx
-        self.rect.y += dy
-        """Check for collisions with walls, if we hit a wall, move away from wall"""
-        for wall in wall_list:
-            if self.rect.colliderect(wall.rect):
-                if dx > 0:
-                    self.rect.right = wall.rect.left
-                if dx < 0:
-                    self.rect.left = wall.rect.right
-                if dy > 0:
-                    self.rect.bottom = wall.rect.top
-                if dy < 0:
-                    self.rect.top = wall.rect.bottom
-# class for the walls
-class Wall(object):
-    def __init__(self, pos):
-        wall_list.append(self)
-        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
-        wall_pos_list.append((self.rect.x, self.rect.y))
-# class for floor
-class Floor(object):
-    def __init__(self, pos):
-        floor_list.append(self)
-        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
-        floor_pos_list.append((self.rect.x, self.rect.y))
-# class for Junk
-class Junk(object):
-    def __init__(self, pos):
-        junk_list.append(self)
-        self.rect = pg.Rect(pos[0], pos[1], 20, 20)
-        junk_pos_list.append((self.rect.x, self.rect.y))
-# Adding the individual objects to the relevant list
-wall_list = []
-wall_pos_list = []
-floor_list = []
-floor_pos_list = []
-dalek_list = []
-dalek_pos_list = []
-junk_list = []
-junk_pos_list = []
-# -------- Main Program Loop -----------
-class Game():
-    # creates a map from a user-selected file
-    # criteria for valid file selection
-    # Player instructions
+        for i, dalek in enumerate(Dalek.dalek_list):
+            if dalek.rect.x < self.rect.x:
+                dalek.move(20, 0)
+            if dalek.rect.x > self.rect.x:
+                dalek.move(-20, 0)
+            if dalek.rect.y < self.rect.y:
+                dalek.move(0, 20)
+            if dalek.rect.y > self.rect.y:
+                dalek.move(0, -20)
+
+
+# -------- Game Functions and classes--------
+def teleport(player):
+    t_pos = list(set(Floor.floor_pos_list) - set(Junk.junk_pos_list))
+    new_pos = rnd.choice(t_pos)
+    player.rect.x = new_pos[0]
+    player.rect.y = new_pos[1]
+
+
+class LenghtError(Exception):
+    pass
+
+
+class SymbolError(Exception):
+    pass
+
+
+def check_file():
+    """Checks user file and makes sure it is in the right format and in the right directory"""
     log = open("WelcomeScreen", "r").read()
     print(log)
     loop = True
@@ -197,9 +157,18 @@ class Game():
         try:
             file = input("Please select a level: ")
             txt = open(file)
-            MapData = txt.readlines()
+            map_data = txt.readlines()
             txt.close()
+            for i in map_data:
+                if not re.match("^[DA*.#]*$", i):
+                    raise SymbolError
+                elif len(i) != 21:
+                    raise LenghtError
             loop = False
+        except SymbolError:
+            print("Error: Only (D), (A), (*), (.), (#) are allowed!")
+        except LenghtError:
+            print("Error: All lines must be 20 entries long!")
         except FileNotFoundError:
             print("No such file in directory.")
         except PermissionError:
@@ -208,111 +177,109 @@ class Game():
             print("No such file in directory.")
         except SyntaxError:
             print("No such file in directory.")
-    for i in MapData:
-        if not re.match("^[DA*.#]*$", i):
-            raise ValueError("""Error: Only (D), (A), (*), (.), (#) are allowed!
-            Please select a new map.""")
-        elif len(i) != 21:
-            raise ValueError("""Error: All lines must be 20 entries long!
-            Please select a new map.""")
-    # Making the map from the selected file
-    x = 0
-    y = 0
-    id = 0
-    for row in MapData:
-        for col in row:
-            if col == "D":
-                player = Player((x, y))
-            if col == "A":
-                Dalek((x, y), id)
-                id += 1
-            if col == "*":
-                Wall((x, y))
-            if col == ".":
-                Floor((x, y))
-            if col == "#":
-                Junk((x, y))
-            x += 20
-        y += 20
-        x = 0
-    # --- Main event loop
-    CarryOn = True
-    End = False
-    while CarryOn:
-        for event in pg.event.get():  # User did something
-            if event.type == pg.QUIT:  # If user clicked close
-                CarryOn = False     # Flag that we are done so we exit this loop
+
+    return map_data
+
+
+def main():
+    """Makes the level from the selected file"""
+    file = check_file()
+    map_x = 0
+    map_y = 0
+    d_id = 0
+    for row in file:
+        for sym in row:
+            if sym == "*":
+                Wall((map_x, map_y))
+            if sym == ".":
+                Floor((map_x, map_y))
+            if sym == "#":
+                Junk((map_x, map_y))
+            if sym == "A":
+                Dalek((map_x, map_y), d_id)
+                d_id += 1
+            if sym == "D":
+                doctor = Doctor((map_x, map_y))
+            map_x += 20
+        map_y += 20
+        map_x = 0
+
+    # ----------- Main Game Loop -------------
+    clock = pg.time.Clock()
+    carry_on = True
+    end = False
+    while carry_on:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                carry_on = False
             elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_x:     # Pressing the x Key will quit the game
-                    CarryOn = False
-        # Move the player if an arrow key is pressed
-        # For each step taken, dalek moves towards doctor
+                if event.key == pg.K_x:
+                    carry_on = False
             key = pg.key.get_pressed()
-            if not End:
+            if not end:
                 if key[pg.K_LEFT]:
-                    player.move(-20, 0)
+                    doctor.move(-20, 0)
                 if key[pg.K_RIGHT]:
-                    player.move(20, 0)
+                    doctor.move(20, 0)
                 if key[pg.K_UP]:
-                    player.move(0, -20)
+                    doctor.move(0, -20)
                 if key[pg.K_DOWN]:
-                    player.move(0, 20)
-        # Teleport the doctor to a random position on the map, not into a wall, or junk
+                    doctor.move(0, 20)
                 if key[pg.K_s]:
-                    t_pos = list(set(floor_pos_list)-set(junk_pos_list))
-                    new_pos = rnd.choice(t_pos)
-                    player.rect.x = new_pos[0]
-                    player.rect.y = new_pos[1]
-        # --- Drawing code should go here
+                    teleport(doctor)
+        # Drawing
         screen = pg.display.set_mode((400, 400))
-        pg.display.set_caption("Dalek_PreAlpha")
+        pg.display.set_caption("Dalek")
         screen.fill(BLACK)
-        for wall in wall_list:
+        for wall in Wall.wall_list:
             pg.draw.rect(screen, GREEN, wall.rect)
-        for dalek in dalek_list:
+        for dalek in Dalek.dalek_list:
             pg.draw.rect(screen, ORANGE, dalek.rect)
-        for junk in junk_list:
+        for junk in Junk.junk_list:
             pg.draw.rect(screen, RED, junk.rect)
-        pg.draw.rect(screen, WHITE, player.rect)
-        # --- Creates a junk when two daleks collide
+        pg.draw.rect(screen, WHITE, doctor.rect)
+        # Places Junk
         collided = []
-        for d1 in dalek_list:
-            for d2 in dalek_list:
-                if d1.id != d2.id and d1.id not in collided:
+        for d1 in Dalek.dalek_list:
+            for d2 in Dalek.dalek_list:
+                if d1.d_id != d2.d_id and d1.d_id not in collided:
                     if d1.rect == d2.rect:
                         junk = Junk(d1.rect)
-                        junk_list.append(junk)
-                        wall_pos_list.append(junk.rect)
+                        Junk.junk_list.append(junk)
+                        Wall.wall_pos_list.append(junk.rect)
                         collided.append(d1)
                         collided.append(d2)
-                        dalek_list.remove(d1)
-                        dalek_list.remove(d2)
-        # --- Removes a dalek if it collides with junk
-        for junk in junk_list:
-            for dalek in dalek_list:
+                        Dalek.dalek_list.remove(d1)
+                        Dalek.dalek_list.remove(d2)
+
+        # Removes a dalek if it collides with junk
+        for junk in Junk.junk_list:
+            for dalek in Dalek.dalek_list:
                 if dalek.rect.colliderect(junk.rect):
-                    dalek_list.remove(dalek)
-        # --- Wining and Losing
-        for dalek in dalek_list:  # If the player is caught by a dalek, the game will end
-            if player.rect.colliderect(dalek.rect):
-                # If you win or lose the game, display the relevant info
+                    Dalek.dalek_list.remove(dalek)
+        # Wining and Losing
+        for dalek in Dalek.dalek_list:  # If the player is caught by a dalek, the game will end
+            if doctor.rect.colliderect(dalek.rect):
+                # If you lose the game, display the relevant info
                 screen.fill(BLACK)
-                LoseText = font1.render("YOU LOSE", 1, WHITE)
-                screen.blit(LoseText, (200 - LoseText.get_width() // 2, 150))
-                LoseDesc = font2.render("YOU WERE CAUGHT BY A DALEK", 1, WHITE)
-                screen.blit(LoseDesc, (200 - LoseDesc.get_width() // 2, 200))
-                End = True
-        if len(dalek_list) == 0:  # If no Daleks are left the game will end
-            # If you win or lose the game, display the relevant info
+                lose_text = BigFont.render("YOU LOSE", 1, WHITE)
+                screen.blit(lose_text, (200 - lose_text.get_width() // 2, 150))
+                lose_desc = SmallFont.render("YOU WERE CAUGHT BY A DALEK", 1, WHITE)
+                screen.blit(lose_desc, (200 - lose_desc.get_width() // 2, 200))
+                end = True
+        if len(Dalek.dalek_list) == 0:  # If no Daleks are left the game will end
+            # If you win the game, display the relevant info
             screen.fill(BLACK)
-            WinText = font1.render("YOU WIN", 1, WHITE)
-            screen.blit(WinText, (200 - WinText.get_width() // 2, 150))
-            WinDesc = font2.render("ALL DALEKS ARE DEAD", 1, WHITE)
-            screen.blit(WinDesc, (200 - WinDesc.get_width() // 2, 200))
-            End = True
+            win_text = BigFont.render("YOU WIN", 1, WHITE)
+            screen.blit(win_text, (200 - win_text.get_width() // 2, 150))
+            win_desc = SmallFont.render("ALL DALEKS ARE DEAD", 1, WHITE)
+            screen.blit(win_desc, (200 - win_desc.get_width() // 2, 200))
+            end = True
         # flip screen between frames
         pg.display.flip()
         # --- Limit to 60 frames per second
         clock.tick(60)
-    # Once we have exited the main program loop we can stop the game engine:
     pg.quit()
+
+
+main()
